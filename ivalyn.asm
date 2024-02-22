b1ad16e9e31c8a91b49f60a08330745b6515a0bf
Chris <topherblair@gmail.com>
2024-02-21 22:58:12 -0800

    2/21/2024 NIGHT

--- ivalyn.asm
+++ ivalyn.asm
@@ -1,16 +1,42 @@
-    processor 6502
+    PROCESSOR 6502
 ;   include required files with VCS register mapping
-    include "vcs.h"
-    include "macro.h"
+    INCLUDE "vcs.h"
+    INCLUDE "macro.h"
 ; *******************************************************************
 ; THE LABRYNTH OF IVALYN
 ; By CHRIS
 ; *******************************************************************
 ; An action game where the player navigates a maze looking for a magic missle
-; to defeat the female dragon Ivalyn and save the princess...
+; or sword to defeat the female dragon Ivalyn and save the princess...
 ; *******************************************************************
 ; TODO
 ; [] Black Background
-; [] Display Playfield
 ; [] Create Sprite Lookup Tables
 ; [] Set Color Lookup Tables
+; [] Display Mage Locations (Score)
+; [] Display Playfield
+; [] Display Player
+; [] Change Player Equipment
+; [] 2D Array???
+; [] Display Arrow
+; [] Death Screen
+; [] Dragon Hit
+; [] Magic Missle
+; [] Dragon Fire
+;
+; Bugs:
+;
+;
+
+; *******************************************************************
+; Start our ROM code at memory address $F000
+; *******************************************************************
+    SEG code
+    ORG $f000
+
+Reset:
+    CLEAN_START                         ; macro to clear memory
+
+; *******************************************************************
+; Set the main display loop and frame rendering
+; *******************************************************************
