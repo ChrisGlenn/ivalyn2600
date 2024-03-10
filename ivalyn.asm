9611c85264969d0d3110ce9f7a6f06b3c7afc023
Chris <topherblair@gmail.com>
2024-03-09 22:51:07 -0800

    3/9/2024 NIGHT

--- ivalyn.asm
+++ ivalyn.asm
@@ -41,19 +41,40 @@
 ;
 ;
 
-; JUST GET A BLACK SCREEN AND THEN WE WILL GO FROM THERE...
+; *******************************************************************
+; Declare variables starting from memory address $80
+; *******************************************************************
+    SEG.U Variables
+    ORG $80
+
+; *******************************************************************
+; Start our ROM code at memory address $F000
+; *******************************************************************
+    SEG code
     ORG $F000
 
-Reset
-    CLEAN_START
+Reset:
+    CLEAN_START                         ; macro to clear memory
 
-StartFrame
-    LDA #$0E
-    STA COLUBK
+; *******************************************************************
+; Initialize RAM variables and TIA registers
+; *******************************************************************
+    ; LDA #$00                            ; black color
+    ; STA COLUBK                          ; store black into background register
+
+; *******************************************************************
+; Set the main display loop and frame rendering
+; *******************************************************************
+StartFrame:
+    ; game reset using console switch
+    ; LSR SWCHB                           ; console input
+    ; BCC Reset                           ; reset the game if pressed
+
     ; DISPLAY VSYNC AND VBLANK
     LDA #2
     STA VSYNC                           ; turn on vsync
     STA VBLANK                          ; turn on vblank
+
    ; display 3 lines of WSYNC
     LDX #3
 .LoopWSYNC:
@@ -62,6 +83,7 @@ StartFrame
     BNE .LoopWSYNC
     LDA #0
     STA VSYNC                           ; turn off vsync
+
     ; output VBLANK
     LDX #37                             ; x = 37
 .LoopVBlank:
@@ -71,13 +93,22 @@ StartFrame
     LDA #0
     STA VBLANK                          ; turn off VBLANK
 
-    LDX #192
-.VisibleFrame:
-    LDA #0
-    STA WSYNC
-    DEX
-    BNE .VisibleFrame
 
+; *******************************************************************
+; *******************************************************************
+; VISIBLE SCANLINES
+; here's where it gets tricky
+; *******************************************************************
+    LDX #192                            ; 192 scanlines
+.VisibleScanlines:
+    STA WSYNC                           ; wsync
+    DEX                                 ; decrement X
+    BNE .VisibleScanlines                ; if X > 0 then loop back up
+
+; *******************************************************************
+; end of visible scanlines
+; *******************************************************************
+; *******************************************************************
     ; DISPLAY OVERSCAN
     LDA #2
     STA VBLANK                          ; turn ON VBLANK
@@ -89,8 +120,192 @@ StartFrame
     LDA #0
     STA VBLANK                          ; turn OFF VBLANK
 
-    JMP StartFrame                      ; jump back to StartFrame
 
+; ***************************************
+    JMP StartFrame                      ; jump back to StartFrame
+; ***************************************
+
+; *******************************************************************
+; Declare ROM lookup tables
+; *******************************************************************
+Digits:
+    .byte %01110111          ; ### ###
+    .byte %01010101          ; # # # #
+    .byte %01010101          ; # # # #
+    .byte %01010101          ; # # # #
+    .byte %01110111          ; ### ###
+
+    .byte %00010001          ;   #   #
+    .byte %00010001          ;   #   #
+    .byte %00010001          ;   #   #
+    .byte %00010001          ;   #   #
+    .byte %00010001          ;   #   #
+
+    .byte %01110111          ; ### ###
+    .byte %00010001          ;   #   #
+    .byte %01110111          ; ### ###
+    .byte %01000100          ; #   #
+    .byte %01110111          ; ### ###
+
+    .byte %01110111          ; ### ###
+    .byte %00010001          ;   #   #
+    .byte %00110011          ;  ##  ##
+    .byte %00010001          ;   #   #
+    .byte %01110111          ; ### ###
+
+    .byte %01010101          ; # # # #
+    .byte %01010101          ; # # # #
+    .byte %01110111          ; ### ###
+    .byte %00010001          ;   #   #
+    .byte %00010001          ;   #   #
+
+    .byte %01110111          ; ### ###
+    .byte %01000100          ; #   #
+    .byte %01110111          ; ### ###
+    .byte %00010001          ;   #   #
+    .byte %01110111          ; ### ###
+
+    .byte %01110111          ; ### ###
+    .byte %01000100          ; #   #
+    .byte %01110111          ; ### ###
+    .byte %01010101          ; # # # #
+    .byte %01110111          ; ### ###
+
+    .byte %01110111          ; ### ###
+    .byte %00010001          ;   #   #
+    .byte %00010001          ;   #   #
+    .byte %00010001          ;   #   #
+    .byte %00010001          ;   #   #
+
+    .byte %01110111          ; ### ###
+    .byte %01010101          ; # # # #
+    .byte %01110111          ; ### ###
+    .byte %01010101          ; # # # #
+    .byte %01110111          ; ### ###
+
+    .byte %01110111          ; ### ###
+    .byte %01010101          ; # # # #
+    .byte %01110111          ; ### ###
+    .byte %00010001          ;   #   #
+    .byte %01110111          ; ### ###
+
+    .byte %00100010          ;  #   #
+    .byte %01010101          ; # # # #
+    .byte %01110111          ; ### ###
+    .byte %01010101          ; # # # #
+    .byte %01010101          ; # # # #
+
+    .byte %01110111          ; ### ###
+    .byte %01010101          ; # # # #
+    .byte %01100110          ; ##  ##
+    .byte %01010101          ; # # # #
+    .byte %01110111          ; ### ###
+
+    .byte %01110111          ; ### ###
+    .byte %01000100          ; #   #
+    .byte %01000100          ; #   #
+    .byte %01000100          ; #   #
+    .byte %01110111          ; ### ###
+
+    .byte %01100110          ; ##  ##
+    .byte %01010101          ; # # # #
+    .byte %01010101          ; # # # #
+    .byte %01010101          ; # # # #
+    .byte %01100110          ; ##  ##
+
+    .byte %01110111          ; ### ###
+    .byte %01000100          ; #   #
+    .byte %01110111          ; ### ###
+    .byte %01000100          ; #   #
+    .byte %01110111          ; ### ###
+
+    .byte %01110111          ; ### ###
+    .byte %01000100          ; #   #
+    .byte %01100110          ; ##  ##
+    .byte %01000100          ; #   #
+    .byte %01000100          ; #   #
+
+Player
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+
+Goblin
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+
+Hundling
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+
+Dragon
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+
+Rope
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+
+Sword
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+
+Shield
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+    .byte #%00000000        ;
+
+
+; *******************************************************************
+; Complete ROM size to 4k
+; *******************************************************************
     ORG $FFFC                       ; move to position $FFFC
     .word Reset                     ; write 2 bytes with the program reset address
     .word Reset                     ; write 2 bytes with the interruption vector
